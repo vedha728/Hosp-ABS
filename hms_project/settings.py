a1742f897ee7c2538aac24031ce984f3e4a94541
Vedhaganesh K <vedhaganesh17@gmail.com>
2026-06-18 23:28:40 +0530

    fix: move DEBUG and ALLOWED_HOSTS to env

--- hms_project/settings.py
+++ hms_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',') 
 
 # Application definition
 
